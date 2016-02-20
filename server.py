f8c94c7d260ae8c7438be4cee90182a79e6ea042
Eropkin Vitaliy <ist70@mail.ru>
2016-02-20 22:09:41 +0400

    Com1

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
 import socket                                                                   
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                           
 s.bind(('', 2222))                                                     
-s.listen(5)   
-client, addr = s.accept()                                                                  
-while True:                                                                     
-    data = client.recv(1024)  
-    client.settimeout(60)
+s.listen(10)   
+while True:   
+    conn, addr = s.accept()	                                                                  
+    data = conn.recv(1024)  
     if bytes('close'.encode('utf-8')) in data:  
-        client.close()                                                          
+        conn.close()                                                          
         s.close()                                                               
         break                                                                  
     if data:              
-        client.send(data)
-        client.close()
+        conn.send(data)
+        conn.close()
+
 
